27e1621542befc57aa22a05237624060354ff7bb
Diego Pandolfi <diegopandolfi@Diegos-MacBook-Pro-2.local>
2023-05-30 10:01:51 -0300

    Inicio

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Diego Pandolfi Almeida da Conceicao';
   }
 }
